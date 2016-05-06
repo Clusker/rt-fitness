81ccf0c232590931db11804190ff837742e8321a
cluggers <sclusker@hotmail.com>
2016-05-06 01:48:21 +0000

    As good as it can get

---
+++ static/DATABASE/formtable.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS signups;
+
+CREATE TABLE signups (
+    ID integer PRIMARY KEY,
+    FullName text NOT NULL,
+    Email text NOT NULL,
+    SignUp integer NOT NULL
+    );
+   
+   
